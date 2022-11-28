43bc6091f73fea71a9e8e3ecf2143527b140bc59
Mona Lisa <naomiroznik@gmail.com>
2022-11-28 15:05:05 -0500

    more js and npm packages

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,22 @@
-CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    department_id INT,
-    salary DECIMAL,
-    manager_id INT
+DROP DATABASE IF EXISTS employees;
+CREATE database employees;
+
+USE employees;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
+
+
+
+-- CREATE TABLE department (
+--     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     title VARCHAR(30) NOT NULL,
+--     department_id INT,
+--     salary DECIMAL,
+--     manager_id INT
+-- );
---
+++ server.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2');
+const express = require('express');
+const express = require('inquirer');
+const consoleTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'employees'
+    },
+    console.log('Connected to the employees database.')
+  );
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
