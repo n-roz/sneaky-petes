3a43ba1122f67800640ebd97f9c9c107dc57591b
Mona Lisa <naomiroznik@gmail.com>
2022-11-29 15:20:05 -0500

    trying to connect everything in the mysql shell

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS department;
+DROP DATABASE IF EXISTS employee_DB;
+CREATE database employee_DB;
+
+USE employee_DB;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
